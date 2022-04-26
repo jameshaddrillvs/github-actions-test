83ce2b142312593346395915746cb7340c9c2b00
jhaddrill <james.haddrill@visitscotland.com>
2022-04-26 15:10:38 +0100

    Add unit tests

---
+++ tests/unit/main.spec.js
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils';
+import mainComponent from '../../src/App.vue';
+
+test('displays image', () => {
+    const wrapper = mount(mainComponent);
+
+    expect(wrapper.html()).toContain('img')
+});
