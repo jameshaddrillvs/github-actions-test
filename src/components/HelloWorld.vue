6d2cca93ea1f81a3bc34ab729221989b78b106e0
jhaddrill <james.haddrill@visitscotland.com>
2022-04-27 12:13:35 +0100

    change text as a test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      Let's change this text
       <a
         href="https://cli.vuejs.org"
         target="_blank"
